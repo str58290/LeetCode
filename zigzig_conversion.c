fb27b9349df06a1a501e8f1d63e2f55c88bcd0a7
Soh Tai Rong <tairong.soh@comguys.com>
2025-07-25 07:51:25 +0800

    Probably need to correct syntax error

--- zigzig_conversion.c
+++ zigzig_conversion.c
@@ -1,44 +1,46 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 char* convert(char* s, int numRows) {
-    char zigZag[sizeof(s)][numRows];
-    char s_zigZagged[sizeof(s)];
+    char zigZag[strlen(s)][numRows];
     int i = 0; // To control the indexing of the string
     int staggerDistance = numRows - 2;
     int staggerPosition = staggerDistance;
-    bool prevStaggerState, staggerState = false;
+    bool staggerState = false;
 
-    for (int x = 0; x < sizeof(s); x++) { // To control the x direction of the 2D array
-        if (i == sizeof(s)) break; // The moment we hit the end of the string, break out
+    for (int x = 0; x < strlen(s); x++) { // To control the x direction of the 2D array
+        if (i == strlen(s)) break; // The moment we hit the end of the string, break out
 
-        else{ // Figure out how we will enter the stagger (i.e. make staggerActive = true)
+        else { // Figure out how we will enter the stagger (i.e. make staggerActive = true)
             for (int y = 0; y < numRows; y++) { // To control the y direction of the 2D array
                 if (staggerState && y == staggerPosition) { // If stagger is active, and we are in the right stagger position to add in the character, then add it in increment to the next character
                     zigZag[x][y] = s[i];
-                    i++;
+                    i++; // Character from original string added in. Move on to next
                     staggerPosition--;
                 } else if (staggerState && y != staggerPosition) { // If stagger is active, but we are not at the position to add in, just fill in that space of the 2D array with NULL
-                    zigZag[x][y] = NULL;
+                    zigZag[x][y] = '\0';
                 } else { // If stagger is not active, proceed normal way of filling 2D array
-                    if (y < numRows) {
-                        zigZag[x][y] = s[i];
-                        i++;
-                    };
+                    zigZag[x][y] = s[i];
+                    i++;
                 }
             }
-            if (prevStaggerState == false) staggerState = true;
+            if (staggerState == false) staggerState = true; 
+            if (staggerPosition == 0) {
+                staggerState = false;
+                staggerPosition = staggerDistance;
+            }
         }
     }
 
-    return s_zigZagged;
+    return zigZag;
 }
 
 int main() {
     int numRows = 4;
     char s[] = "PAHNAPLSIIGYIR";
 
-    convert(s, numRows);
+    printf("%s\n", convert(s, numRows));
 
     return 0;
 }
